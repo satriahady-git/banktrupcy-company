601b6bf52e6f6f5f2cc3342cfd93744e20d541a8
Satria Hady <satriaholmes@gmail.com>
2026-08-30 01:08:04 +0700

    final_model

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-import dataclasses
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
+class Config:
+    seed: int = 42
+    test_size: float = 0.2
+    target_column: str='Bankrupt?'
 
+config = Config()
